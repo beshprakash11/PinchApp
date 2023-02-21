9d551e0f72a2d34e55ab5d34ba108c454101a208
Besh P.Yogi <beshprakashktm@gmail.com>
2023-02-21 12:50:53 +0100

    Initial pinch app

---
+++ PinchApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PinchApp
+//
+//  Created by Besh P.Yogi on 21.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Besh P.Yogi on 21.02.23.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
