c5f383ecf8029a0e7c14b17c4de45fa17f09d672
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 13:47:46 +0100

    Info panel added

--- PinchApp/Screen/InfoPanelView.swift
+++ PinchApp/Screen/InfoPanelView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct InfoPanelView: View {
+    var scale: CGFloat
+    var offset: CGSize
+    
+    
     var body: some View {
         HStack{
             //MARK: - HOTSPOT
@@ -17,13 +21,29 @@ struct InfoPanelView: View {
                 .frame(width: 30, height: 30)
             Spacer()
             //MARK: INFO PANEL
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+                
+                Spacer()
+            }
         }
     }
 }
 
 struct InfoPanelView_Previews: PreviewProvider {
     static var previews: some View {
-        InfoPanelView()
+        InfoPanelView(scale: 1, offset: .zero)
             .preferredColorScheme(.dark)
             .previewLayout(.sizeThatFits)
             .padding()
