1a435ceacbb42b3100870ad93892153c41d7f080
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 13:25:14 +0100

    Drag gesture added

--- PinchApp/Screen/ContentView.swift
+++ PinchApp/Screen/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     //MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
-    
+    @State private var imageOffset: CGSize = .zero //CGSize(width: 0, height: 0)
     //MARK: - FUNCTION
     //MARK: - CONTENT
     var body: some View {
@@ -25,6 +25,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2),radius: 12, x: 2, y:2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 //MARK: - 1. TAP Gesture
                     .onTapGesture(count: 2, perform: {
@@ -38,6 +39,25 @@ struct ContentView: View {
                             }
                         }
                     })
+                //MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged{value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded{_ in
+                                if imageScale <= 1{
+                                    withAnimation(.spring()){
+                                        imageScale = 1
+                                        imageOffset = .zero
+                                    }
+                                }else{
+                                    
+                                }
+                            }
+                    )               
                 
                 
             }
