fb518eb8ed0092db8f3f59070af731f27336acb9
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 13:12:06 +0100

    Tap gesture with image scale added

--- PinchApp/Screen/ContentView.swift
+++ PinchApp/Screen/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     //MARK: - PROPERTY
     @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
     
     //MARK: - FUNCTION
     //MARK: - CONTENT
@@ -24,6 +25,19 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2),radius: 12, x: 2, y:2)
                     .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                //MARK: - 1. TAP Gesture
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1{
+                            withAnimation(.spring()){
+                                imageScale = 5
+                            }
+                        }else{
+                            withAnimation(.spring()){
+                                imageScale = 1
+                            }
+                        }
+                    })
                 
                 
             }
