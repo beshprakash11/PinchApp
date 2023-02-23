5fdcf430657ddbbd2e87895b1d49b8321d2f46ea
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 13:08:40 +0100

    implicit animating added

--- PinchApp/Screen/ContentView.swift
+++ PinchApp/Screen/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     //MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
+    
     //MARK: - FUNCTION
     //MARK: - CONTENT
     var body: some View {
@@ -21,9 +23,17 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2),radius: 12, x: 2, y:2)
+                    .opacity(isAnimating ? 1 : 0)
+                
+                
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating = true
+                }
+            })
         }
         .navigationViewStyle(.stack)
     }
