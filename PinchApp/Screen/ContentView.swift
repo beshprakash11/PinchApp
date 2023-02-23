721f73cd218a792292862f4da5b5fc98d91f14ef
beshprakash11 <beshprakashktm@gmail.com>
2023-02-23 13:05:28 +0100

    Navigation and magazine front cover added

--- PinchApp/Screen/ContentView.swift
+++ PinchApp/Screen/ContentView.swift
@@ -12,13 +12,20 @@ struct ContentView: View {
     //MARK: - FUNCTION
     //MARK: - CONTENT
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack{
+                //MARK: PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2),radius: 12, x: 2, y:2)
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        .navigationViewStyle(.stack)
     }
 }
 
