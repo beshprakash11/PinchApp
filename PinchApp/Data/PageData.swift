386c0b69c909300d172e5a7fa3915fee9c8c38ef
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 13:37:58 +0100

    Page data added

---
+++ PinchApp/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  PinchApp
+//
+//  Created by Besh P.Yogi on 24.02.23.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
