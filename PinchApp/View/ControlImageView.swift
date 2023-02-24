fc308be87100776dfe795e8495b16eac9dd5dda9
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 12:36:51 +0100

    Controlimage view added

---
+++ PinchApp/View/ControlImageView.swift
@@ -0,0 +1,23 @@
+//
+//  ControlImageView.swift
+//  PinchApp
+//
+//  Created by Besh P.Yogi on 24.02.23.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
