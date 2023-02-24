647a504f2d25e6f976abeb8291a47a088c474e2d
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 12:53:22 +0100

    magnifaction gesture updated

--- PinchApp/Screen/ContentView.swift
+++ PinchApp/Screen/ContentView.swift
@@ -63,6 +63,18 @@ struct ContentView: View {
                 //MARK: - 3. MAGNIFICATION
                     .gesture(
                         MagnificationGesture()
+                            .onChanged{value in
+                                withAnimation(.linear(duration: 1)){
+                                    if imageScale >= 1 && imageScale <= 5{
+                                        imageScale = value
+                                    }else if imageScale > 5{
+                                        imageScale = 5
+                                    }else{
+                                        
+                                    }
+                                }
+                                
+                            }
                     )
                 
                 
