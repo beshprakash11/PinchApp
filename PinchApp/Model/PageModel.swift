fa5222303cd57e6b5590d83008caa1049abef690
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 13:35:23 +0100

    Page model added and updated

---
+++ PinchApp/Model/PageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PageModel.swift
+//  PinchApp
+//
+//  Created by Besh P.Yogi on 24.02.23.
+//
+
+import Foundation
+
+struct Page: Identifiable{
+    let id: Int
+    let imageName: String
+}
